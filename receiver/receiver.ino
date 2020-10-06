899505e9b23aa6f24a9ce9e76d9a6f54b44adb91
JRC - Tech <javier.ruzzante@gmail.com>
2020-10-05 22:03:41 -0400

    Added comments to the code

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -6,7 +6,7 @@
 int i, good, k;
 byte data_in;
 void setup(){
-  attachInterrupt(1,data_incoming,RISING);
+  attachInterrupt(1,data_incoming,RISING);    //Interrupt on the rising edge of the signal at the input pin
   pinMode(3, INPUT);
   pinMode(13, OUTPUT);
   Serial.begin(115200);
@@ -19,43 +19,41 @@ void loop(){
 
 void data_incoming(){
     
-    
-    for(i=0; i<100; i++){
+    //Filter for noise and training pulses...
+    for(i=0; i<100; i++){    //This loop ensures that the input pin is high for at least 2000us.  So, this will discard all the training pulses and good=1 when the pin is in the long high (3ms) sent by the transmitter.
       delayMicroseconds(20);
       good=1;
-      if(digitalRead(3)==LOW){
+      if(digitalRead(3)==LOW){  //If the pin is low before 2ms then jump off.
       good=0;
       i=100;
       }
     }//for
-      
+
+    //Here we have detected the long high, and after switch off interrupt, wait for the input pin becomes low.  When this occur, transmitter and receiver are sync  
     if(good==1){
     detachInterrupt(1);
     while(1){
-      if(digitalRead(3)==LOW){
-        delayMicroseconds(750);
+      if(digitalRead(3)==LOW){    //Sync!!
+        delayMicroseconds(750);   //Now, wait 500us (low pulse duration) + 250us, (half pulse duration of the first data bit).  So, this will give an offset for checking the input at the middle of the pulse.
 
-        for(i=0; i<8; i++){
+        for(i=0; i<8; i++){       //Start receiving the 8 bits, checking every 1000us (it has the 250us offset already when did the delay...)
           if(digitalRead(3)==HIGH)
-          bitWrite(data_in, i, 1);
+            bitWrite(data_in, i, 1);
           else
-          bitWrite(data_in, i, 0);
+            bitWrite(data_in, i, 0);
           delayMicroseconds(1000);
         }//for
-        if(data_in=='#')
+        if(data_in=='#')        //This is a special char used to indicate the end of the message transmission
         Serial.println("");
         else
         Serial.print(char(data_in));
 
-       break;//secondtwhile
+       break;//while
       }//low kickoff
       
     }//second while
     
     }//good check
 
-  attachInterrupt(1,data_incoming,RISING);
-  
-  
-  
+  attachInterrupt(1,data_incoming,RISING);  
 }//routine
